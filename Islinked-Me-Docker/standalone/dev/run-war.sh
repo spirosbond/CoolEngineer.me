1cc8468f2fb745615de5659eccec12e340f3c043
spirosbond <spirosbond@hotmail.com>
2023-03-05 23:28:27 +0100

    added Dockerfiles

---
+++ Islinked-Me-Docker/standalone/dev/run-war.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "Starting war"
+# java -server -Xmx768M -Dgrails.env=prod -Dserver.port=8080 -Dmyserver.url='localhost:8080' -jar ./CoolEngineer-me-0.3.1.war
+java -server -Xmx768M -Dgrails.env=prod -Dserver.port=8080 -Dmyserver.url='dev.islinked.me' -DdataSource.url='jdbc:mysql://172.28.0.7:3306/islinked-me' -jar /root/ROOT.war
+ 
